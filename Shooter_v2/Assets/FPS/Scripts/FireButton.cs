7b531ada211a8a7b71b08947d063dab1df0878dd
Rshetty02 <70402171+Rshetty02@users.noreply.github.com>
2020-11-07 00:00:19 +0530

    Shooter game with touch input

---
+++ Shooter_v2/Assets/FPS/Scripts/FireButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FireButton : MonoBehaviour
+{   
+
+
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        //PlayerInputHandler.fireButton = true;
+        //Debug.Log("Button Pressed");
+    }
+
+    public void UIfire(){
+    {
+
+    //PlayerInputHandler.fireButtonUI = true;
+    //Debug.Log("Button Pressed");
+
+    }
+        
+    
+
+   
+    
+    
+
+
+
+
+}
+}
